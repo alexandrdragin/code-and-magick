ffbcdc8ccaf094467dea96ae2d61161ff9200826
AlexandrDragin <one.eastside@gmail.com>
2015-09-23 19:43:13 +0300

    Сократил код в 2 двое
    вместо не работающей валидации добавил полям аттрибут required
    
    по прежнему не работают куки у радио
    
    и скрытия review-fields-label

--- js/form.js
+++ js/form.js
@@ -6,11 +6,27 @@ var formOpenButton = document.querySelector('.reviews-controls-new');
 var formCloseButton = document.querySelector('.review-form-close');
 
 
+
 var reviewForm = document.querySelector('.review-form');
 var reviewName = document.getElementById('review-name');
 var reviewText = document.getElementById('review-text');
-
 var reviewScoreValue = reviewForm.querySelector('input[name="review-mark"]:checked').value;
+
+
+reviewName.required = true;
+reviewText.required = true;
+
+
+if (reviewName.length > 1) {
+  var reviewFieldsName = document.getElementsByClassName('review-fields-label review-fields-name');
+  reviewFieldsName.style.display = "display";
+};
+
+if (reviewText.length > 1) {
+var reviewFieldsText = document.getElementsByClassName('review-fields-label review-fields-text');
+reviewFieldsText.style.display = "none";
+};
+
 
 
 function setCookieFun (name, value, expires) {
@@ -21,63 +37,27 @@ reviewScoreValue.value = docCookies.getItem('radioValCook');
 reviewName.value = docCookies.getItem('reviewNameCook');
 
 
-function validateForm() {
-var bad = [];
-if (reviewName.length < 3) {
-    bad.push('Имя слишком короткое');
+
+
+reviewForm.onsubmit = function(e) {
+e.preventDefault();
+var now = new Date();
+var exDate = new Date(now.getTime() + (30 * 365 * 60 * 60 * 24 * 1000));
+
+var reviewScoreValue = reviewForm.querySelector('input[name="review-mark"]:checked').value;
+var currentName = reviewName.value;
+
+    setCookieFun('radioValCook', reviewScoreValue.value, 'exDate.toUTCString()');
+    setCookieFun('reviewNameCook', currentName, 'exDate.toUTCString()');
+
+
+    reviewForm.submit();
     }
-    if (reviewName.length > 32) {
-      bad.push('Имя слишком длинное');
-    }
-    if (reviewText.length < 3) {
-    bad.push('Напишите побольше пожалуйста');
-    }
-    if (reviewText.length > 52) {
-      bad.push('Напишите поменьше пожалуйста');
-   }
-    if (reviewText.length === 0 || reviewText === " ") {
-      bad.push('Вы забыли написать что хотели');
-    }
-    if (reviewName.length === 0 || reviewName === " ") {
-      bad.push('имя забыли');
-      alert(bad.join('\n'));
-    }
-    return !bad.length
-  }
-
-
-    reviewForm.onsubmit = function(e) {
-      e.preventDefault();
-      var now = new Date();
-      var exDate = new Date(now.getTime() + (30 * 365 * 60 * 60 * 24 * 1000));
-
-      var reviewScoreValue = reviewForm.querySelector('input[name="review-mark"]:checked').value;
-      var currentName = reviewName.value;
 
 
 
-          setCookieFun('radioValCook', reviewScoreValue.value, 'exDate.toUTCString()');
-          setCookieFun('reviewNameCook', currentName, 'exDate.toUTCString()');
-          }
-
-          var isValid = validateForm();
-          if (isValid) {formContainer.classList.add('invisible');}
 
 
-      if (reviewName.length > 0) {
-        var reviewFieldsName = document.getElementsByClassName('review-fields-name');
-        reviewFieldsName.style.visibility = "hidden";
-  };
-
-
-      if (reviewText.length > 0) {
-      var reviewFieldsText = document.getElementsByClassName('review-fields-text');
-      reviewFieldsText.style.visibility = "hidden";
-      };
-
-
-
-///
 
 
   //появление формы
