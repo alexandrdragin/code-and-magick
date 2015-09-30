17847782ab3515bf3019096bc4493d6121a9a3b1
AlexandrDragin <one.eastside@gmail.com>
2015-09-30 15:30:25 +0300

    Все выровнил все работает, начинаю над след заданием работать

--- js/form.js
+++ js/form.js
@@ -1,85 +1,84 @@
 'use strict';
 
 //вызов ананимной функции
-(function() {
-  var formContainer = document.querySelector('.overlay-container');
-  var formOpenButton = document.querySelector('.reviews-controls-new');
-  var formCloseButton = document.querySelector('.review-form-close');
+(function () {
+    var formContainer = document.querySelector('.overlay-container');
+    var formOpenButton = document.querySelector('.reviews-controls-new');
+    var formCloseButton = document.querySelector('.review-form-close');
 
-  var reviewForm = document.querySelector('.review-form');
-  var reviewName = document.getElementById('review-name');
-  var reviewText = document.getElementById('review-text');
+    var reviewForm = document.querySelector('.review-form');
+    var reviewName = document.getElementById('review-name');
+    var reviewText = document.getElementById('review-text');
 
-  reviewName.required = true;
-  reviewText.required = true;
+    reviewName.required = true;
+    reviewText.required = true;
 
-  function checkValues() {
-    var reviewNameLabel = reviewForm.getElementsByClassName('review-fields-name')[0];
-    if (reviewName.value.length) {
-      reviewNameLabel.classList.add("invisible");
-      } else {
-      reviewNameLabel.classList.remove("invisible");
-      };
-
-      var reviewFieldsText = reviewForm.getElementsByClassName('review-fields-text')[0];
-      if (reviewText.value.length) {
-        reviewFieldsText.classList.add("invisible");
+    function checkValues() {
+        var reviewNameLabel = reviewForm.getElementsByClassName('review-fields-name')[0];
+        if (reviewName.value.length) {
+            reviewNameLabel.classList.add("invisible");
         } else {
-          reviewFieldsText.classList.remove("invisible");
+            reviewNameLabel.classList.remove("invisible");
+        }
+        var reviewFieldsText = reviewForm.getElementsByClassName('review-fields-text')[0];
+        if (reviewText.value.length) {
+            reviewFieldsText.classList.add("invisible");
+        } else {
+            reviewFieldsText.classList.remove("invisible");
         };
-      };
+    };
 
-reviewName.onkeyup = function() {
-  checkValues();
-  };
+    reviewName.onkeyup = function () {
+        checkValues();
+    };
 
-reviewText.onkeyup = function() {
-  checkValues();
-  };
-
-
-function setCookieFun (name, value, expires) {
-  document.cookie = name + "=" + escape(value) + ((expires) ? "; expires=" + expires : "");
-  };
+    reviewText.onkeyup = function () {
+        checkValues();
+    };
 
 
-reviewName.value = docCookies.getItem('reviewNameCook');
-
-var savedScoreValue = docCookies.getItem('radioValCook');
-if (savedScoreValue) {
-var selectedRadio = reviewForm.querySelector('input[name="review-mark"][value="' + savedScoreValue +'"]');
-selectedRadio.setAttribute('checked', true);
-};
+    function setCookieFun (name, value, expires) {
+        document.cookie = name + "=" + escape(value) + ((expires) ? "; expires=" + expires : "");
+    };
 
 
-reviewForm.onsubmit = function(e) {
-  e.preventDefault();
-  var now = new Date();
-  var exDate = new Date(now.getTime() + (30 * 365 * 60 * 60 * 24 * 1000));
+    reviewName.value = docCookies.getItem('reviewNameCook');
 
-  var reviewScoreValue = reviewForm.querySelector('input[name="review-mark"]:checked').value;
-  var currentName = reviewName.value;
-
-    setCookieFun('radioValCook', reviewScoreValue, exDate.toUTCString());
-    setCookieFun('reviewNameCook', currentName, exDate.toUTCString());
+    var savedScoreValue = docCookies.getItem('radioValCook');
+    if (savedScoreValue) {
+        var selectedRadio = reviewForm.querySelector('input[name="review-mark"][value="' + savedScoreValue + '"]');
+        selectedRadio.setAttribute('checked', true);
+    };
 
 
-    reviewForm.submit();
-    }
+    reviewForm.onsubmit = function (e) {
+        e.preventDefault();
+        var now = new Date();
+        var exDate = new Date(now.getTime() + (30 * 365 * 60 * 60 * 24000));
+
+        var reviewScoreValue = reviewForm.querySelector('input[name="review-mark"]:checked').value;
+        var currentName = reviewName.value;
+
+        setCookieFun('radioValCook', reviewScoreValue, exDate.toUTCString());
+        setCookieFun('reviewNameCook', currentName, exDate.toUTCString());
+
+
+        reviewForm.submit();
+    };
 
 
   //появление формы
-     formOpenButton.onclick = function(evt) {
-       evt.preventDefault();
-       formContainer.classList.remove('invisible');
-     };
+    formOpenButton.onclick = function (evt) {
+        evt.preventDefault();
+        formContainer.classList.remove('invisible');
+    };
 
 
   // скрытие формы
-     formCloseButton.onclick = function(evt) {
-       evt.preventDefault();
-       formContainer.classList.add('invisible');
-     };
+    formCloseButton.onclick = function (evt) {
+        evt.preventDefault();
+        formContainer.classList.add('invisible');
+    };
 
 
- })();
+}) ();
