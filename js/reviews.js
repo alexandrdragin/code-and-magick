8b0ff5d4cac0656e023119586d34c26e3f8e9df3
AlexandrDragin <one.eastside@gmail.com>
2015-10-05 19:34:19 +0300

    задание 4
    постраничная загрузка

--- js/reviews.js
+++ js/reviews.js
@@ -36,7 +36,7 @@
   //  фрагмент для ускорения загрузки
   var reviewsFragment = document.createDocumentFragment();
 
-  var reviews;
+  var originalReviews;
   var currentPage = 0;
 
   reviewForm.classList.remove('invisible');
@@ -47,10 +47,10 @@
 
     reviewContainer.classList.remove('invisible');
     // чистим контейнер
-    reviewContainer.innerHTML='';
+    reviewContainer.innerHTML = '';
 
     // выбираем размер страницы
-    var reviewsFrom =  pageNumber * pageSize;
+    var reviewsFrom = pageNumber * pageSize;
     var reviewsTo = reviewsFrom + pageSize;
 
     // и перезаписываем ее с таким размером слайсом
@@ -115,7 +115,6 @@
           if (loadedXhr.status === 200 || loadedXhr.status === 304) {
             var data = loadedXhr.response;
             reviewContainer.classList.remove('reviews-list-loading');
-            // console.log(data);
         //  хелпер переводящий в формат джейсона
             callback(JSON.parse(data));
           }
@@ -133,7 +132,6 @@
     xhr.onerror = function() {
       showLoadFailure();
     };
-
 //  закрытие функции загрузки по xhr
   }
 
@@ -149,16 +147,17 @@
     switch (filterID) {
       case 'reviews-recent':
         filteredReviews = filteredReviews.sort(function(a, b) {
-          //  вообщем дата вот в таком формате 2013-12-03 не очень понимаю к чему ее привести нужно чтобы сравнивать
-          if (a.date > b.date || (b.date && a.date === 'underfined')) {
+          var firstDate = (new Date(a.date)).valueOf();
+          var secondDate = (new Date(b.date)).valueOf();
+          if (firstDate > secondDate) {
             return -1;
           }
 
-          if (a.date < b.date || (b.date && a.date === 'underfined')) {
+          if (firstDate < secondDate || (secondDate && firstDate === 'undefined')) {
             return 1;
           }
 
-          if (a.date === b.date) {
+          if (firstDate === firstDate) {
             return 0;
           }
         });
@@ -166,16 +165,15 @@
         break;
 
       case 'reviews-good':
-
         filteredReviews = filteredReviews.filter(function(a) {
           return a.rating > 3;
         });
         filteredReviews = filteredReviews.sort(function(a, b) {
-          if (a.rating > b.rating || (b.rating && a.rating === 'underfined')) {
+          if (a.rating > b.rating) {
             return -1;
           }
 
-          if (a.rating < b.rating || (b.rating && a.rating === 'underfined')) {
+          if (a.rating < b.rating || (b.rating && a.rating === 'undefined')) {
             return 1;
           }
 
@@ -191,11 +189,11 @@
           return a.rating < 3;
         });
         filteredReviews = filteredReviews.sort(function(a, b) {
-          if (a.rating > b.rating || (b.rating && a.rating === 'underfined')) {
+          if (a.rating > b.rating) {
             return 1;
           }
 
-          if (a.rating < b.rating || (b.rating && a.rating === 'underfined')) {
+          if (a.rating < b.rating || (b.rating && a.rating === 'undefined')) {
             return -1;
           }
 
@@ -207,17 +205,16 @@
         break;
 
       case 'reviews-popular':
-
         filteredReviews = filteredReviews.sort(function(a, b) {
-          if (a.review-rating > b.review-rating || (b.review-rating && a.review-rating === 'underfined')) {
+          if (a['review-rating'] > b['review-rating'] || (b['review-rating'] && a['review-rating'] === 'undefined')) {
             return 1;
           }
 
-          if (a.review-rating < b.review-rating || (b.review-rating && a.review-rating === 'underfined')) {
+          if (a['review-rating'] < b['review-rating']) {
             return -1;
           }
 
-          if (a.review-rating === b.review-rating) {
+          if (a['review-rating'] === b['review-rating']) {
             return 0;
           }
         });
@@ -239,11 +236,7 @@
       // добовлям обработчик события которая запускает сетАктивФильтер
       filterElements[i].onclick = function(evt) {
         var clickedFilter = evt.currentTarget;
-        setActiveFilter(clickedFilter.id);
-        // и чекед переставляет местами
-        document.querySelector('.reviews-filter-item.checked').setAttribute('checked', false);
-        document.querySelector('input[name="reviews"]').setAttribute('checked', false);
-        move('.reviews-filter-item.checked');
+        setActiveFilter(clickedFilter.getAttribute('for'));
         clickedFilter.setAttribute('checked', true);
       };
     }
@@ -251,51 +244,28 @@
 
   //  функция включающая сортировку берет список ревью фильтурет по правилам
   function setActiveFilter(filterID) {
-    var filteredReviews = filterReviews(reviews, filterID);
+    var filteredReviews = filterReviews(originalReviews, filterID);
     //  возвращаем и отрисовываем
     loadingReviews(filteredReviews, currentPage);
   }
 
   startFilters();
 
-  loadingReviews(function(loadedReviews) {
-    reviews = loadedReviews;
-    setActiveFilter('reviews-all');
-  });
+  // loadingReviews(function(loadedReviews) {
+  //   originalReviews = loadedReviews;
+  //   setActiveFilter('reviews-all');
+  // });
 
 // когда загрузилось эта функция принимает data, сохраняет и отрисовывает их
 // loadingReviews();
   loadXHR(function(loadedReviews) {
-    reviews = loadedReviews;
-  //  loadingReviews = (loadedReviews);
+    originalReviews = loadedReviews;
+    loadingReviews(loadedReviews);
   });
 
 })();
 
 /*
-<form class="reviews-filter" action="index.html" method="get">
-  <input type="radio" name="reviews" id="reviews-all" value="reviews-all" checked><label for="reviews-all" class="reviews-filter-item"> Все</label>
-  <input type="radio" name="reviews" id="reviews-recent" value="reviews-recent"><label for="reviews-recent" class="reviews-filter-item"> Недавние</label>
-  <input type="radio" name="reviews" id="reviews-good" value="reviews-good"><label for="reviews-good" class="reviews-filter-item"> Хорошие</label>
-  <input type="radio" name="reviews" id="reviews-bad" value="reviews-bad"><label for="reviews-bad" class="reviews-filter-item"> Плохие</label>
-  <input type="radio" name="reviews" id="reviews-popular" value="reviews-popular"><label for="reviews-popular" class="reviews-filter-item"> Популярные</label>
-
-
-/*
-Доработайте модуль js/reviews.js:
-+Отключите загрузку данных из файла data/reviews.js убрав подключение этого скрипта из index.html.
-+Загрузите данные из файла data/reviews.json по XMLHttpRequest.
-+Пока длится загрузка файла, покажите прелоадер, добавив класс .reviews-list-loading блоку .reviews.
-+Когда загрузка закончится, уберите прелоадер и покажите список отзывов, как в предыдущем задании.
-+Если загрузка закончится неудачно (ошибкой сервера или таймаутом), покажите предупреждение об ошибке, добавив блоку .reviews класс reviews-load-failure.
-
-Напишите обработчики событий для фильтров, так, чтобы они фильтровали загруженный список отзывов следующим образом:
-  Все — показывает список отзывов в таком виде, в котором он был загружен.
-  Недавние — показывает список отзывов, оставленных за последние полгода, отсортированных по убыванию даты (поле date).
-  Хорошие — с рейтингом не ниже 3, отсортированные по убыванию рейтинга (поле rating).
-  Плохие — с рейтингом не выше 2, отсортированные по возрастанию рейтинга.
-  Популярные — отсортированные по убыванию оценки отзыва (поле reviewRating).
-
 ---------------------
 Задача
 
