f2f3741083df961792b3fd7850b1e1c0a9b0983e
AlexandrDragin <one.eastside@gmail.com>
2015-10-14 17:53:19 +0300

    задание 5.1 файл  review.js баглайн на 56 строке
    Привет, задание состояло в том чтобы разделить весь код на 2 файла и
    перекроить все с помощью прототипов. и у меня вообщем то получилось,
    только до изображений авторов не могу добраться.
    Uncaught TypeError: Cannot read property 'author' of undefined
    
    не знаю как текущее содержимое this._data проверить и путь правильно
    указать
    
    все остальное ок

--- js/review.js
+++ js/review.js
@@ -1,76 +1,62 @@
 'use strict';
 
 (function() {
-  /**
-   * @const
-   * @type {number}
-   */
-  var requestFailureTimeout = 10000;
 
   /**
    * @type {Object.<string, string>}
    */
    //  мап для раздвижки звезд по css
-     var ratingClass = {
-       '1': 'review-rating-one',
-       '2': 'review-rating-two',
-       '3': 'review-rating-three',
-       '4': 'review-rating-four',
-       '5': 'review-rating-five'
-     };
+  var ratingClass = {
+    '1': 'review-rating-one',
+    '2': 'review-rating-two',
+    '3': 'review-rating-three',
+    '4': 'review-rating-four',
+    '5': 'review-rating-five'
+  };
 
   /**
    * @type {Element}
    */
    //  шаблон для загрузки
-   var reviewTemplate = document.getElementById('review-template');
+  var reviewTemplate = document.getElementById('review-template');
 
   /**
-   * Конструктор объектов типа Hotel. Кроме создания объекта, добавляет каждому объекту
+   * Конструктор объектов типа Review. Кроме создания объекта, добавляет каждому объекту
    * свойство data_ и фиксирует контекст у обработчика события клика.
    * @constructor
    * @param {Object} data
    */
-  var Rewiew = function(data) {
+  var Review = function(data) {
     this._data = data;
+    this._element = null;
 
     // Фиксирование контекста обработчика. При любом вызове this._onClick, объект отеля
     // будет использоваться как контекст (даже при вызове через call и apply).
-    this._onClick = this._onClick.bind(this);
+    // this._onClick = this._onClick.bind(this);
   };
 
   /**
    * Создание DOM-элемента, отрисовка его в переданный контейнер и добавление обработчика события клика.
    * По большей части, не отличается от ранее написанного кода, кроме способа обращения к данным. Теперь
-   * они берутся не из аргумента итератора, а хранятся в объекте Hotel в свойстве data_.
+   * они берутся не из аргумента итератора, а хранятся в объекте Review в свойстве data_.
    * @param  {Element|DocumentFragment} container
    */
   Review.prototype.render = function(container) {
-    // Клонирование нового объекта для отеля из шаблона и заполнение его реальными
+    // Клонирование нового объекта для Review из шаблона и заполнение его реальными
     // данными, взятыми из свойства data_ созданного конструктором.
-    var newHotelElement = hotelTemplate.content.children[0].cloneNode(true);
-    var amenitiesContainer = newHotelElement.querySelector('.hotel-amenities');
+    var newReviewData = reviewTemplate.content.children[0].cloneNode(true);
 
-    newHotelElement.querySelector('.hotel-stars').classList.add(starsClassName[this._data['stars']]);
-    newHotelElement.querySelector('.hotel-name').textContent = this._data['name'];
-    newHotelElement.querySelector('.hotel-distance-kilometers').textContent = [this._data['distance'], 'км'].join(' ');
-    newHotelElement.querySelector('.hotel-price-value').textContent = this._data['price'];
-    newHotelElement.querySelector('.hotel-rating').textContent = this._data['rating'];
-    newHotelElement.querySelector('.hotel-rating').classList.add(ratingClassName[Math.floor(this._data['rating'])]);
-
-    this._data['amenities'].forEach(function(amenity) {
-      var amenityElement = document.createElement('li');
-      amenityElement.classList.add('hotel-amenity');
-      amenityElement.classList.add(amenityClassName[amenity]);
-      amenitiesContainer.appendChild(amenityElement);
-    });
+    newReviewData.querySelector('.review-rating').classList.add(ratingClass[this._data['rating']]);
+    newReviewData.querySelector('.review-text').textContent = this._data['description'];
+    newReviewData.querySelector('.review-author').title = this._data['author']['name'];
 
     // Добавление в контейнер.
-    container.appendChild(newHotelElement);
+    container.appendChild(newReviewData);
 
-    // Добавление фонового изображения.
-  var authorImages = newReviewData.querySelector('.review-author');
-  if (this._data['preview']) {
+///////////////////////////////////////////// БАГ ЛАЙН
+
+    if (this._data['author']['picture']) {
+      var authorImages = newReviewData.querySelector('.review-author');
       var tempImages = new Image();
       tempImages.onload = function() {
         authorImages.src = this._data['author']['picture'];
@@ -82,11 +68,12 @@
         authorImages.remove();
       };
 
-      tempImages.src = review['author']['picture'];
+      tempImages.src = this._data['author']['picture'];
 
-    this._element = newReviewElement;
-    // Обработчик клика
-    this._element.addEventListener('click', this._onClick);
+      this._element = newReviewData;
+      // Обработчик клика
+      this._element.addEventListener('click', this._onClick);
+    }
   };
 
   /**
@@ -98,27 +85,30 @@
     this._element = null;
   };
 
-  /**
-   * Обработчик события клика по элементу отеля. Проверяет, отсутствует ли у элемента класс hotel-nophoto
-   * и если да, то создает кастомное событие showgallery с добавочными данными в свойстве detail, которые
-   * указывают на текущий объект отеля. Это используется для передачи фотографий отеля в фотогалерею.
-   * @private
-   */
-  Review.prototype._onClick = function() {
-    if (!this._element.classList.contains('hotel-nophoto')) {
-      var galleryEvent = new CustomEvent('showgallery', { detail: { hotelElement: this }});
-      window.dispatchEvent(galleryEvent);
-    }
-  };
 
   /**
-   * Возвращает список фотографий текущего отеля, получив его из объекта data_.
+   * Возвращает список фотографий текущего Review, получив его из объекта data_.
    * @return {Array.<string>}
    */
   Review.prototype.getPhotos = function() {
     return this._data.pictures;
   };
 
-  // Экспорт конструктора объекта Hotel в глобальную область видимости.
+  // Экспорт конструктора объекта Review в глобальную область видимости.
   window.Review = Review;
+
 })();
+
+/*
+Создайте модуль js/review.js, в котором опишите объект типа Review и сделайте его доступным
+  в глобальной области видимости.
+Конструктор объекта Review должен принимать на вход аргумент data и сохранять его
+  в приватном свойстве _data.
+Объект Review должен обладать следующими свойствами и методами:
+Метод render который занимается отрисовкой элемента отзыва в списке
+  и добавлением ему обработчиков событий.
+Метод unrender который убирает элемент из списка и удаляет обработчики событий.
+Отредактируйте модуль js/reviews.js так, чтобы он отрисовывал отзывы,
+  создавая объекты типа Review, а при фильтрации, сделайте удаление отзывов,
+  вызовом метода unrender у каждого отзыва.
+*/
