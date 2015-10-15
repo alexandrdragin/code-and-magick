aa293338cc27684c40304b20a038c4b1298b0035
AlexandrDragin <one.eastside@gmail.com>
2015-10-15 17:52:55 +0300

    Новый блок gallery c объект типа Gallery
    основной блок написан но не работает(наверно) из за  проблем с
    итерацией (c 36 строки)

--- js/gallery.js
+++ js/gallery.js
@@ -9,6 +9,170 @@
     'RIGHT': 39
   };
 
+// создает конструктор галереи со свойствами
+
+  var Gallery = function() {
+    // блок со всеми фото
+    this.photogalleryContainer = document.querySelector('.photogallery');
+    //блок оверлея
+    this.element = document.body.querySelector('.overlay-gallery');
+    // блок с номером фото
+    this._pictureElement = this.element.querySelector('.overlay-gallery-preview');
+    // кнопки
+    this._closeButtton = this.element.querySelector('.overlay-gallery-close');
+    this._leftButton = this.element.querySelector('.overlay-gallery-control-left');
+    this._rightButton = this.element.querySelector('.overlay-gallery-control-right');
+
+    this._currentPhoto = 0;
+    this._photos = [];
+    // привязывалка
+    this._onCloseClick = this._onCloseClick.bind(this);
+    this._onLeftButtonClick = this._onLeftButtonClick.bind(this);
+    this._onRightButtonClick = this._onRightButtonClick.bind(this);
+    this._onKeyDown = this._onKeyDown.bind(this);
+  }
+
+  /**
+   * Записывает список фотографий.
+   * @param {Array.<string>} photos
+   */
+  Gallery.prototype.setPhotos = function(photos) {
+    this.photogalleryContainer.forEach.call() = function() {
+      this._photos = photos;
+      var imageElement = new Image();
+      imageElement.src = this._photos[this._currentPhoto];
+    }
+  };
+
+  /**
+     * Показывает фотогалерею, убирая у контейнера класс invisible. Затем добавляет
+     * обработчики событий и показывает текущую фотографию.
+     */
+  Gallery.prototype.showGallery = function() {
+    this.element.classList.remove('invisible');
+
+    this._closeButtton.addEventListener('click', this._onCloseClick);
+    this._leftButton.addEventListener('click', this._onLeftButtonClick);
+    this._rightButton.addEventListener('click', this._onRightButtonClick);
+    document.body.addEventListener('keydown', this._onKeyDown)
+
+    this._showCurrentPhoto();
+  }
+  /**
+     * Наоборот, доб у контейнера класс invisible. Затем убирает
+     * обработчики событий и обнуляет текущую фотографию.
+     */
+  Gallery.prototype.hideGallery = function() {
+    this.element.classList.add('invisible');
+    this._closeButtton.removeEventListener('click', this._onCloseClick);
+    document.body.removeEventListener('keydown', this._onKeyDown);
+
+    this._currentPhoto = 0;
+    this._photos = [];
+  }
+
+  Gallery.prototype._onClick = function(evt) {
+    this.photogalleryContainer.addEventListener('click', this._onClick);
+    if (doesHaveParent(evt.target, 'photogallery' )) {
+      this.showGallery();
+    }
+  };
+
+  /**
+   * Приватный метод, показывающий текущую фотографию. Убирает предыдущюю
+   * отрисованную фотографию, создает объект Image с src указанным
+   * в массиве photos_ под индексом currentPhoto_ и после загрузки показывает
+   * его на странице.
+   * @private
+   */
+  Gallery.prototype._showCurrentPhoto = function() {
+    this._pictureElement.innerHTML = '';
+
+    var imageElement = new Image();
+    imageElement.src = this._photos[this._currentPhoto];
+    imageElement.onload = function() {
+      this._pictureElement.appendChild(imageElement);
+    }.bind(this);
+  };
+
+  /**
+   * Обработчик события клика по крестику закрытия. Вызывает метод hide.
+   * @param {Event} evt
+   * @private
+   */
+  Gallery.prototype._onCloseClick = function(evt) {
+    evt.preventDefault();
+    this.hide();
+  };
+
+  /**
+   * Обработчик события клика по стрелке влево.
+   * @param {Event} evt
+   * @private
+   */
+  Gallery.prototype._onLeftButtonClick = function(evt) {
+    evt.preventDefault();
+    this.setCurrentPhoto(this._currentPhoto - 1);
+  };
+
+  /**
+   * Обработчик события клика по стрелке вправо.
+   * @param {Event} evt
+   * @private
+   */
+  Gallery.prototype._onRightButtonClick = function(evt) {
+    evt.preventDefault();
+    this.setCurrentPhoto(this._currentPhoto + 1);
+  };
+
+  /**
+   * Обработчик клавиатурных событий. Прячет галерею при нажатии Esc
+   * и переключает фотографии при нажатии на стрелки.
+   * @param {Event} evt
+   * @private
+   */
+  Gallery.prototype._onKeyDown = function(evt) {
+    switch (evt.keyCode) {
+      case Key.ESC:
+        this.hide();
+        break;
+
+      case Key.LEFT:
+        this.setCurrentPhoto(this._currentPhoto - 1);
+        break;
+
+      case Key.RIGHT:
+        this.setCurrentPhoto(this._currentPhoto + 1);
+        break;
+    }
+  };
+
+
+    /**
+     * Устанавливает номер фотографии, которую нужно показать, предварительно
+     * "зажав" его между 0 и количеством фотографий в галерее минус 1 (чтобы нельзя
+     * было показать фотографию номер -1 или номер 100 в массиве из четырех
+     * фотографий), и показывает ее на странице.
+     * @param {number} index
+     */
+    Gallery.prototype.setCurrentPhoto = function(index) {
+      index = clamp(index, 0, this._photos.length - 1);
+
+      if (this._currentPhoto === index) {
+        return;
+      }
+
+      this._currentPhoto = index;
+      this._showCurrentPhoto();
+    };
+
+  window.Gallery = Gallery;
+
+})();
+
+
+
+/*
   var photogalleryContainer = document.querySelector('.photogallery');
   var gallery = document.querySelector('.overlay-gallery');
   var closeButtton = gallery.querySelector('.overlay-gallery-close');
@@ -63,18 +227,4 @@
     }
   });
 
-
-})();
-
-/**
- Создайте модуль js/gallery.js и реализуйте в нем базовый функционал для фотогалереи.
-
-+Добавьте с помощью делегирования обработчик кликов по фотографиям в галерее,
-+который убирает класс invisible у блока .overlay-gallery.
-+Когда блок .overlay-gallery появляется, должен добавляться обработчик клавиатурных событий:
-+Нажатие на Esc должно закрывать блок.
-+Нажатия на стрелки влево и вправо должны вызывать функции переключения слайдов галереи.
-Сами функции пока что реализовывать не нужно, достаточно чтобы эти функции выводили в консоль направление переключения.
-+Добавьте обработчик клика по крестику в блоке .overlay-gallery-close, который будет скрывать этот блок.
-+Когда блок .gallery-overlay скрывается, обработчики событий должны удаляться.
 */
