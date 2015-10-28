e311feb35210ee1a6604a66144b18f6b796a4b92
AlexandrDragin <one.eastside@gmail.com>
2015-10-28 18:57:25 +0300

    КАВЫЧИ

--- js/game.js
+++ js/game.js
@@ -380,77 +380,77 @@ define(function() {
       switch (this.state.currentStatus) {
         case Verdict.WIN:
           console.log('you have won!');
-          this.ctx.fillStyle="rgba(0, 0, 0, 0.7)";
+          this.ctx.fillStyle='rgba(0, 0, 0, 0.7)';
           this.ctx.fillRect(210, 30, 300, 150);
-          this.ctx.fillStyle="white";
+          this.ctx.fillStyle='white';
           this.ctx.fillRect(200, 20, 300, 150);
           this.ctx.fill();
 
-          this.ctx.font = "bold 30px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillStyle = "Black";
+          this.ctx.font = 'bold 30px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillStyle = 'Black';
           this.ctx.fillText("Ты выйграл!", 350, 80);
 
-          this.ctx.font = "normal normal 16px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillText("Пробел если хочешь выиграть еще", 350, 120);
+          this.ctx.font = 'normal normal 16px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillText('Пробел если хочешь выиграть еще', 350, 120);
           break;
 
         case Verdict.FAIL:
           console.log('you have failed!');
-          this.ctx.fillStyle="rgba(0, 0, 0, 0.7)";
+          this.ctx.fillStyle='rgba(0, 0, 0, 0.7)';
           this.ctx.fillRect(210, 30, 300, 150);
-          this.ctx.fillStyle="white";
+          this.ctx.fillStyle='white';
           this.ctx.fillRect(200, 20, 300, 150);
           this.ctx.fill();
 
-          this.ctx.font = "bold 30px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillStyle = "Black";
-          this.ctx.fillText("Слил", 320, 100);
+          this.ctx.font = 'bold 30px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillStyle = 'Black';
+          this.ctx.fillText('Слил', 320, 100);
 
-          this.ctx.font = "normal normal 16px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillText("Пробел для попробывать еще", 350, 100);
+          this.ctx.font = 'normal normal 16px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillText('Пробел для попробывать еще', 350, 100);
 
           break;
         case Verdict.PAUSE:
           console.log('game is on pause!');
-          this.ctx.fillStyle="rgba(0, 0, 0, 0.7)";
+          this.ctx.fillStyle='rgba(0, 0, 0, 0.7)';
           this.ctx.fillRect(210, 30, 300, 150);
-          this.ctx.fillStyle="white";
+          this.ctx.fillStyle='white';
           this.ctx.fillRect(200, 20, 300, 150);
           this.ctx.fill();
 
-          this.ctx.font = "bold 30px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillStyle = "Black";
-          this.ctx.fillText("Пауза", 350, 80);
+          this.ctx.font = 'bold 30px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillStyle = 'Black';
+          this.ctx.fillText('Пауза', 350, 80);
 
-          this.ctx.font = "normal normal 16px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillText("потому что вы отскролили вниз", 350, 110);
-          this.ctx.fillText("Пробел чтобы продолжить", 350, 140);
+          this.ctx.font = 'normal normal 16px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillText('потому что вы отскролили вниз', 350, 110);
+          this.ctx.fillText('Пробел чтобы продолжить', 350, 140);
           break;
 
         case Verdict.INTRO:
           console.log('welcome to the game! Press Space to start');
-          this.ctx.fillStyle="rgba(0, 0, 0, 0.7)";
+          this.ctx.fillStyle='rgba(0, 0, 0, 0.7)';
           this.ctx.fillRect(210, 30, 300, 150);
-          this.ctx.fillStyle="white";
+          this.ctx.fillStyle='white';
           this.ctx.fillRect(200, 20, 300, 150);
           this.ctx.fill();
 
-          this.ctx.font = "bold 16px PT Mono";
-          this.ctx.textBaseline = "Top";
-          this.ctx.fillStyle = "Black";
-          this.ctx.fillText("Правила просты", 280, 50);
+          this.ctx.font = 'bold 16px PT Mono';
+          this.ctx.textBaseline = 'Top';
+          this.ctx.fillStyle = 'Black';
+          this.ctx.fillText('Правила просты', 280, 50);
 
-          this.ctx.font = "normal normal 16px PT Mono";
-          this.ctx.textAlign = "center";
-          this.ctx.fillText("Стрелки помогают для ходьбы", 350, 100);
-          this.ctx.fillText("Шифтом можно выйграть", 350, 120);
-          this.ctx.fillText("Пробел для старта", 350, 140);
+          this.ctx.font = 'normal normal 16px PT Mono';
+          this.ctx.textAlign = 'center';
+          this.ctx.fillText('Стрелки помогают для ходьбы', 350, 100);
+          this.ctx.fillText('Шифтом можно выйграть', 350, 120);
+          this.ctx.fillText('Пробел для старта', 350, 140);
 
           break;
         case Verdict.FAIL:
